783ca091982f5776d1bc08b9892a75d0fdadfbcd
DragonMaster <ms235389@gmail.com>
2020-11-04 13:07:55 +0530

    Updates

---
+++ precache-manifest.dc6cde83020c631c8d1c41b69ba0e720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230f217893b2807096c9b833eaa45b4d",
+    "url": "/React-Contact-Manager/index.html"
+  },
+  {
+    "revision": "1a33d6dc2fb1962c45c3",
+    "url": "/React-Contact-Manager/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "8c58ae6b369df8b495e6",
+    "url": "/React-Contact-Manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1a33d6dc2fb1962c45c3",
+    "url": "/React-Contact-Manager/static/js/2.42bbb3a9.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/React-Contact-Manager/static/js/2.42bbb3a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c58ae6b369df8b495e6",
+    "url": "/React-Contact-Manager/static/js/main.9583f6c6.chunk.js"
+  },
+  {
+    "revision": "c5df9fe54deefc7c5640",
+    "url": "/React-Contact-Manager/static/js/runtime-main.181a3630.js"
+  }
+]);
